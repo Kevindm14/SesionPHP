b496fb5c113a3b91702107c4f6914e2647015663
Kevin Diaz <kevindiazm.14@gmail.com>
2019-03-18 01:33:34 -0500

    login form

--- registro.php
+++
@@ -1,3 +0,0 @@
-<?php 
-    require 'views/registro.view.php';
-?>
---
+++ views/login.view.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/estilos.css">
+    <title>Sign up</title>
+</head>
+<body>
+    <div class="contenedor">
+        <div class="item">
+            <h1 class="titulo">Sign up</h1>
+            <hr class="border">
+            <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST" class="form" name="register">
+                <div class="form-group">
+                    <i class="icono izquierda fa fa-user"></i><input type="text" class="usuario" name="name" placeholder="Write you name">
+                </div>
+                <div class="form-group">
+                    <i class="icono izquierda fa fa-lock"></i><input type="password" class="password" name="password" placeholder="Write you password">
+                    <i class="submit-btn fa fa-arrow-right" onclick="login.submit()"></i>
+                </div>
+            </form>
+            <p class="text-login">
+                You are not yet registered?
+                <a href="registro.php">Sign in</a>
+            </p>
+        </div>
+    </div>
+</body>
+</html>
