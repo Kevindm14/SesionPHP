84edafd08e766a9b78c2d1b98dafa58d5f190b3c
Kevin Diaz <kevindiazm.14@gmail.com>
2019-03-18 01:19:33 -0500

    register form

---
+++ views/contenido.view.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/estilos.css">
+    <title>Contenido</title>
+</head>
+<body>
+    <div class="contenedor">
+        <div class="item">
+            <h1 class="titulo">Contenido del sitio</h1>
+            <hr class="border">
+            <a href="cerrar.php">Cerrar Sesion</a>
+            <div class="contenido">
+                <article>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente qui, reprehenderit, sint perspiciatis, libero accusamus beatae dolore in amet tempore totam! At, aliquid perspiciatis unde a quisquam dolores. Ipsa, cum.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta maiores quisquam distinctio magni sint inventore quos debitis earum neque possimus? Asperiores odio modi impedit voluptatem laboriosam, in ipsam suscipit eos.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur recusandae fugit pariatur, rem, et, enim numquam amet sunt harum velit magni tenetur illo sit dolores eos molestias perspiciatis provident. Neque?</p>
+                </article>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
