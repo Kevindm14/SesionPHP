bde6acc4ac6aa623c836d75de2b44f7858dc83a1
Kevin Diaz <kevindiazm.14@gmail.com>
2019-03-19 00:28:26 -0500

    custom validations

--- register.php
+++ register.php
@@ -13,6 +13,38 @@
 
         if (empty($name) or empty($password) or empty($password2)) {
             $errors .= "<li>Fill in the fields correctly</li>";
+        } else {
+            try {
+                $conection = new PDO('mysql:host=localhost;dbname=sesion;', 'root', 'localhost');
+            } catch (PDOException $e) {
+                echo "Error" . $e->getMessage();
+            }
+
+            // Validation Username 
+            $statement = $conection->prepare('SELECT * FROM users WHERE name = :name LIMIT 1');
+            $statement->execute(array(':name' => $name));
+            $result = $statement->fetch();
+            if ($result != false) {
+                $errors .= "<li>This username already exists</li>";
+            }
+
+            // Validation Password
+            $password = hash('sha512', $password);
+            $password2 = hash('sha512', $password2);
+
+            if ($password != $password2) {
+                $errors .= "<li>Passwords are not equal</li>";
+            }
+        }
+
+        if ($errors == '') {
+            $statement = $conection->prepare('INSERT INTO users (id, name, password) VALUES(null, :name, :password)');
+            $statement->execute(array(
+                ':name' => $name, 
+                ':password' => $password)
+            );
+
+            header('Location: login.php');
         }
     }
 
